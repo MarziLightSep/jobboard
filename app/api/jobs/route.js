c538c203da0a522e65adddfb8da0c72e65960072
marzi <marzilightsep24@gmail.com>
2026-08-23 18:03:56 +0330

    connect Mongoose to local MongoDB, fetch jobs from database

--- app/api/jobs/route.js
+++ app/api/jobs/route.js
@@ -1,5 +1,9 @@
-import { jobs } from "@/app/data/jobs";
+import { connectDB } from "@/lib/mongodb";
+import Job from "@/models/Job";
+
 
 export async function GET() {
+    await connectDB();
+    const jobs = await Job.find();
     return Response.json(jobs);
 }
---
+++ models/Job.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.models.Job || mongoose.model("Job", JobSchema);
